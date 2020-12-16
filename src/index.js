2bf5adf44fb630f22feb71f7084d41d59f2b597a
matthewogtong <ogtongm@gmail.com>
2020-12-15 23:19:28 -0500

    complete main deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  const userTaskForm = document.querySelector('#create-task-form')
+  const userTaskDescription = document.querySelector('#new-task-description')
+  const userToDoList = document.querySelector('#tasks') 
+
+  userTaskForm.addEventListener('submit', createNewTask)
+
+})
+
+const createNewTask = function(event) {
+  console.log(event)
+  event.preventDefault()
+
+  const userToDoList = document.querySelector('#tasks') 
+  const userTaskDescription = document.querySelector('#new-task-description')
+
+  const newTask = userTaskDescription.value
+  const li = document.createElement('li')
+  li.textContent = newTask
+  userToDoList.append(li)
+  event.target.reset()
+}
